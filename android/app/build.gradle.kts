a1481b54d97a8665622868868bddb56de81a0a2b
Sanskar <sanskarin@outlook.in>
2026-08-14 15:53:07 +0530

    build: finish Android launch branding

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,11 +15,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.sanskarin.puzzleforge"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
